83c4326bc601e1e3acd061e525e457495183a153
Sina4229 <124515502+Sina4229@users.noreply.github.com>
2023-02-06 14:11:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -33,7 +33,8 @@ async def on_member_join(member):
 async def on_message(message):
     if message.content.startswith ("!인증 "):
         if message.author.guild_permissions.administrator:
-            await message.delete()
+            await message.channel.send(embed=discord.Embed(title="인증 시스템", description = "인증이 정상적으로 완료 되었습니다 !",timestamp=datetime.datetime.now(pytz.timezone('UTC')), color = 0xff0000))
+
             user = message.mentions[0]
 
             embed = discord.Embed(title="인증 시스템", description="인증이 정상적으로 완료 되었습니다 !",timestamp=datetime.datetime.now(pytz.timezone('UTC')), color=0xff0000)
@@ -46,9 +47,7 @@ async def on_message(message):
             await user.add_roles(role)
 
         else:
-            await message.delete()
             await message.channel.send(embed=discord.Embed(title="권한 부족", description = message.author.mention + "님은 권한이 없습니다", color = 0xff0000))
-
 
 
 try:
