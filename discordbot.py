aa8d0a11cfb3d67add3c772edb6f2896fa25ea82
Sina4229 <124515502+Sina4229@users.noreply.github.com>
2023-02-14 19:05:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -50,7 +50,7 @@ async def on_message(message):
             await message.channel.send(embed=discord.Embed(title="권한 부족", description = message.author.mention + "님은 권한이 없습니다", color = 0xff0000))
             
     if message.content.startswith ("!도박꾼 "):
-        if message.author.guild_permissions.administrator:
+        if message.author.guild_permissions.View Channels:
             await message.channel.send(embed=discord.Embed(title="도박 시스템", description = "역할 지급이 정상적으로 완료 되었습니다 !",timestamp=datetime.datetime.now(pytz.timezone('UTC')), color = 0xff0000))
 
             user = message.mentions[0]
